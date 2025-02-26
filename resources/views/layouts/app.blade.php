b831f42d19eaa0c0988552ce58df90816f4c6689
jonatasndossantos <jonatasndossantos@gmail.com>
2025-02-26 17:22:40 -0300

    BD

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,52 +1,41 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png')}}">
 
-        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" media="(prefers-color-scheme: dark)"
+        href="{{URL ('assets/media/favicon-32x32-dark-mode.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" media="(prefers-color-scheme: dark)"
+        href="{{URL ('assets/media/favicon-16x16-dark-mode.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" media="(prefers-color-scheme: light)"
+        href="{{URL ('assets/media/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" media="(prefers-color-scheme: light)"
+        href="{{URL ('assets/media/favicon-16x16.png')}}">
 
-        <link rel="icon" type="image/png" sizes="32x32" media="(prefers-color-scheme: dark)"
-                href="{{URL ('assets/media/favicon-32x32-dark-mode.png')}}">
-        <link rel="icon" type="image/png" sizes="16x16" media="(prefers-color-scheme: dark)"
-                href="{{URL ('assets/media/favicon-16x16-dark-mode.png')}}">
-        <link rel="icon" type="image/png" sizes="32x32" media="(prefers-color-scheme: light)"
-                href="{{URL ('assets/media/favicon-32x32.png')}}">
-        <link rel="icon" type="image/png" sizes="16x16" media="(prefers-color-scheme: light)"
-                href="{{URL ('assets/media/favicon-16x16.png')}}">
+    <link rel="stylesheet" href="{{ url ('/assets/app-DDAX_FZV.css')}}">
 
+</head>
+@include('layouts.navigation')
 
-
-        <link rel="stylesheet" href="{{ url ('/assets/app-DDAX_FZV.css')}}">
-
-    </head>
+<!-- Page Heading -->
+@if (isset($header))
 
 
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+{{ $header }}
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+@endif
+
+<!-- Page Content -->
+<main>
+    {{ $slot }}
+</main>
+
 </html>
